eb9e9292d40494ea98dad0a722716049ba4c1d9b
Sharo <redshanks@Sharos-MacBook-Pro.local>
2024-03-23 01:34:14 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+// const nextConfig = {};
 
 
 
-module.exports = {
+
+
+// export default nextConfig;
+
+
+
+const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        // matching a specific API route
+        source: "/api/driver",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.sameertinc.com" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
     ]
-  } 
+  }
 };
 
-
-
-
-export default nextConfig;
-
-
-
+module.exports = nextConfig;
