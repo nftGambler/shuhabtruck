4f5927dba89a9644eca840ce72c9c8146d07668a
Sharo <redshanks@Sharos-MacBook-Pro.local>
2024-03-22 23:30:29 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,9 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
-// const nextConfig = {};
-
-
-
-// export default nextConfig;
+const nextConfig = {};
 
 
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          // matching a specific API route
-          source: "/api/driver",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    }
-  };
-  
-  export default nextConfig;
-  
+
+export default nextConfig;
 
 
